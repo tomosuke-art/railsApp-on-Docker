ea91c0bfac81d97c2b9568868440c8aa88fe8bcf
tomosuke-art <tomo1130taro@icloud.com>
2020-10-04 00:23:26 +0900

    update travis and compose

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,8 +4,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
   end
-
-  test "should get index" do
+ #それぞれのURLにちゃんとアクセスできるか
+  test "should get index" do 
     get products_url
     assert_response :success
   end
